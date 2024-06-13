d31864ccc9e235227bdb847df906b00973f3f926
wiKanok <kanokwan1401.ks@gmail.com>
2024-06-13 13:37:21 +0700

    feat: wi-react-hook-complete

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,25 +1,11 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import useBlogPosts from "./useBlogPosts";
 
 function HomePage() {
   const navigate = useNavigate();
 
-  const [posts, setPosts] = useState([]);
-  const [isError, setIsError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-
-  const getPosts = async () => {
-    try {
-      setIsError(false);
-      setIsLoading(true);
-      const results = await axios("http://localhost:4000/posts");
-      setPosts(results.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      setIsError(true);
-    }
-  };
+  const { posts, isError, isLoading, getPosts } = useBlogPosts();
 
   useEffect(() => {
     getPosts();
--- client/src/pages/ViewPostPage.jsx
+++ client/src/pages/ViewPostPage.jsx
@@ -1,36 +1,32 @@
-import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useBlogPosts from "./useBlogPosts";
 import axios from "axios";
 
 function ViewPostPage() {
   const navigate = useNavigate();
+  const params = useParams();
 
-  const [posts, setPosts] = useState([]);
-  const [isError, setIsError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
+  const { posts, isError, isLoading, getPosts } = useBlogPosts();
+  const [postById, setPostById] = useState({});
 
-  const getPosts = async () => {
-    try {
-      setIsError(false);
-      setIsLoading(true);
-      const results = await axios("http://localhost:4000/posts");
-      setPosts(results.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      setIsError(true);
-    }
+  const getPostById = async () => {
+    const result = await axios.get(`http://localhost:4000/posts/${params.id}`);
+    setPostById(result.data.data);
+    // console.log(result);
   };
 
   useEffect(() => {
     getPosts();
+    getPostById();
   }, []);
 
   return (
     <div>
       <h1>View Post Page</h1>
       <div className="view-post-container">
-        <h2>Post Title</h2>
-        <p>Content</p>
+        <h2>{postById.title}</h2>
+        <p>{postById.content}</p>
       </div>
 
       <hr />
